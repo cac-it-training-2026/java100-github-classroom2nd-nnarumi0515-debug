99d71a55afc7584ee7119f42b4a881cc498b5e0e
narumina <narumina@C010422720.zipangu.cac.co.jp>
2026-05-14 20:48:22 +0900

    0514 Lesson4,6,7,9完了しました。

--- Java100_questions_cac2nd/src/lesson06/challenge11/Astronaut.java
+++ Java100_questions_cac2nd/src/lesson06/challenge11/Astronaut.java
@@ -39,35 +39,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 //ここにGammalianクラスを記述する
+class Gammalian {
 
+	private String item;//itemフィールド
+	private boolean isGood;//boolean型のフィールド
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {//setterメソッド
+		boolean isHit = false;//boolean型isHitにfalse代入（0～9の数字が見つかってない状態）
+		for (int i = 0; i < 10; i++) {//10回繰り返す
+			if (item.indexOf(Integer.toString(i)) != -1) {//Integer.toStringでiを文字列に変換
+				//indexOf()で文字列itemの中に指定の文字列が最初に出現する位置を返す
+				//見つからない時-1を返す
+				isHit = true;//数字が見つかった時
+				break;//ループ抜ける
+			}
+		}
+		if (isHit) {//isHitがtrueの時
+			isGood = true;//isGoodもtrue
+		} else {
+			isGood = false;
+		}
+		this.item = item;//クラスのフィールドに代入
+	}
+
+	public boolean isGood() {
+		return isGood;
+	}
+
+	public void setGood(boolean isGood) {
+		this.isGood = isGood;
+	}
+}
 
 public class Astronaut {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.print("γ星人にアイテムを渡してください＞");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String present = br.readLine();
+		System.out.print("γ星人にアイテムを渡してください＞");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String present = br.readLine();
 
+		//ここに適切な処理を記述する
+		Gammalian gammalian = new Gammalian();
+		gammalian.setItem(present);
 
-        //ここに適切な処理を記述する
+		boolean isGood = false;
 
+		//ここに適切な処理を記述する
+		isGood = gammalian.isGood();
 
-        boolean isGood = false;
-
-
-        //ここに適切な処理を記述する
-
-
-        System.out.println("\nγ星人：");
-        if(isGood){
-             System.out.println("こんな良いものをもらっていいガンマか！");
-             System.out.println("ゆっくりしていくガンマ。");
-        } else{
-             System.out.println("...ありがとガンマ。");
-             System.out.println("ぶぶ漬けでもいかがガンマか？");
-        }
-    }
+		System.out.println("\nγ星人：");
+		if (isGood) {
+			System.out.println("こんな良いものをもらっていいガンマか！");
+			System.out.println("ゆっくりしていくガンマ。");
+		} else {
+			System.out.println("...ありがとガンマ。");
+			System.out.println("ぶぶ漬けでもいかがガンマか？");
+		}
+	}
 }
